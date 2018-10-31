6b1043b7c715352b89d99bab6d3516ee62afaa4d
Stephen Anderson <anderssj@usc.edu>
2018-10-31 12:32:41 -0700

    done

--- Drills 2.js
+++ Drills 2.js
@@ -1,54 +1,54 @@
-// const loaf = {
-//   flour: 30,
-//   water: 210,
-//   hydration: function() {
-//     return (loaf.water / loaf.flour) * 100
-//   }
-// }
+const loaf = {
+  flour: 30,
+  water: 210,
+  hydration: function() {
+    return (loaf.water / loaf.flour) * 100
+  }
+}
 
-// console.log(loaf.flour)
-// console.log(loaf.water)
-// console.log(loaf.hydration())
+console.log(loaf.flour)
+console.log(loaf.water)
+console.log(loaf.hydration())
 
-// const obj = { foo: 1, bar: 2, fum: 3, quux: 4, spam: 5 }
+const obj = { foo: 1, bar: 2, fum: 3, quux: 4, spam: 5 }
 
-// for (const key in obj) {
-//   if (obj.hasOwnProperty(key)) {
-//     const element = obj[key]
-//     console.log(`${key} ${element}`)
-//   }
-// }
+for (const key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    const element = obj[key]
+    console.log(`${key} ${element}`)
+  }
+}
 
-// const hobbit = {
-//   meals: [
-//     'breakfast',
-//     'second breakfast',
-//     'elevenses',
-//     'lunch',
-//     'afternoon tea',
-//     'dinner',
-//     'supper'
-//   ]
-// }
-// console.log(hobbit.meals[3])
+const hobbit = {
+  meals: [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ]
+}
+console.log(hobbit.meals[3])
 
-// obj1 = { name: 'steve', jobTitle: 'software enigeer', boss: 'james' }
-// obj2 = { name: 'fred', jobTitle: 'developer', boss: 'james' }
-// obj3 = { name: 'james', jobTitle: 'programmer' }
+obj1 = { name: 'steve', jobTitle: 'software enigeer', boss: 'james' }
+obj2 = { name: 'fred', jobTitle: 'developer', boss: 'james' }
+obj3 = { name: 'james', jobTitle: 'programmer' }
 
-// array = [obj1, obj2, obj3]
+array = [obj1, obj2, obj3]
 
-// array.forEach(element => {
-//   if (!element.hasOwnProperty('boss')) {
-//     console.log(
-//       `${element.jobTitle} ${element.name} doesn't report to anybody.`
-//     )
-//   } else {
-//     console.log(
-//       `${element.jobTitle} ${element.name} reports to ${element.boss}.`
-//     )
-//   }
-// })
+array.forEach(element => {
+  if (!element.hasOwnProperty('boss')) {
+    console.log(
+      `${element.jobTitle} ${element.name} doesn't report to anybody.`
+    )
+  } else {
+    console.log(
+      `${element.jobTitle} ${element.name} reports to ${element.boss}.`
+    )
+  }
+})
 
 function createCharacter(name, nickname, race, origin, attack, defense) {
   const character = {}
@@ -130,7 +130,22 @@ const Arwen = createCharacter(
 )
 characterArray.push(legolas, gandalf, bilbo, frodo, aragorn, Arwen)
 
-console.log(gandalf.evaluateFight(bilbo))
+console.log(gandalf.evaluateFight(Arwen))
+
+const arwen = characterArray.find(function(element) {
+  return element.nickname === 'arwen'
+})
+console.log(arwen.describe())
+
+let hobbits = characterArray.filter(function(element) {
+  return element.race === 'Hobbit'
+})
+console.log(hobbits)
+
+let charactersAboveFive = characterArray.filter(function(element) {
+  return element.attack > 5
+})
+console.log(charactersAboveFive)
 
 // ===============================================================================================
 // | Name                      | Nickname    | Race       | Origin         | Attack   | Defense  |
